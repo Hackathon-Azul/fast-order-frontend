90213cbb31a7b020a9b4263db1cb1e2d0a91c5bf
Christian Herber <christianfaldox@gmail.com>
2022-02-17 23:58:07 -0400

    delete some images and modify some files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Pedido Rápido</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
