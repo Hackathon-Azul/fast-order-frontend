b464072b08e42ad107c9a20448ab0e60d38073a4
Christian Herber <christianfaldox@gmail.com>
2022-02-17 23:54:25 -0400

    create Logo component

---
+++ components/Logo/index.jsx
@@ -0,0 +1,17 @@
+
+
+const Logo = () => {
+  return (
+    <>
+      <img
+        className="my-4"
+        src="/logo.png"
+        alt="Pedido Rápido"
+        width={103}
+        height={97}
+      />
+    </>
+  )
+}
+
+export default Logo
