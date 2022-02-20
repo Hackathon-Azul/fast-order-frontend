975663ef4dde7d5875bcbf0c19d58072f59b487b
Christian Herber <christianfaldox@gmail.com>
2022-02-20 02:29:44 -0400

    create tables page

---
+++ pages/tables.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Header from "../components/Header"
+import TableContainer from "../components/TableContainer"
+
+const Tables: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <TableContainer />
+    </>
+  )
+}
+
+export default Tables
