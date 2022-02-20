8de4bb93891539b6db26279c45c077e117ae5c74
Christian Herber <christianfaldox@gmail.com>
2022-02-20 02:28:20 -0400

    create TableContainer component

---
+++ components/TableContainer/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const TableContainerTag = styled.div`
+  width: 90%;
+  max-width: 900px;
+  margin: 0 auto;
+  background-color: rgba(255,255,255,0.1);
+  padding: 5px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`
