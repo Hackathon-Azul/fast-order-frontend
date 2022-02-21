1d7327d79a4a3d29d25a20021507eb2681b18658
João Otávio <joaotavioos@outlook.com>
2022-02-21 09:32:35 -0300

    Add reducers

---
+++ store/modules/rootReducer.ts
@@ -0,0 +1,17 @@
+import { combineReducers } from "redux";
+
+import auth from "./auth/reducer";
+import search from "./admin/shared/search/reducer";
+import category from "./admin/category/reducer";
+import product from './admin/product/reducer';
+import cartProducts from './storefront/cartProducts/reducer';
+import dashboard from './admin/dashboard/reducer';
+
+export default combineReducers({
+  auth,
+  search,
+  category,
+  product,
+  cartProducts,
+  dashboard
+});
