cfd953e74d1036994c57c493f5168dd58eb7bcf9
João Otávio <joaotavioos@outlook.com>
2022-02-28 18:54:14 -0300

    Makes signin and signup forms responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,39 @@
-import type { NextPage } from 'next'
-import styled from 'styled-components'
-import Logo from '../components/Logo'
-import FormSignIn from '../components/FormSignIn'
+import type { NextPage } from "next";
+import styled from "styled-components";
+import Logo from "../components/Logo";
+import FormSignIn from "../components/FormSignIn";
+import media from "styled-media-query";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <>
       <Container>
-        <Logo />
+        <Logo size="large" />
 
         <h5 className="my-4 pb-4">Nome do Restaurante</h5>
 
         <FormSignIn />
-
+        <div className="my-4 py-4">
+          <Link href="/signup">
+            <a className="text-white">Não tem cadastro?</a>
+          </Link>
+        </div>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const Container = styled.div`
-
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 650px;
+  ${media.lessThan("medium")`
+      width:125%;
+    `}
+`;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -4,12 +4,13 @@ import styled from 'styled-components'
 import Logo from '../components/Logo'
 import FormSignUp from '../components/FormSignUp'
 import Link from 'next/link'
+import media from "styled-media-query";
 
 const SingUp: NextPage = () => {
   return (
     <>
-      <Container>
-        <Logo />
+      <Container style={{ marginTop: "15px"}}>
+        <Logo size="large" />
 
         <h2 className="my-4">Seja bem vindo</h2>
         <p>Cadastre-se</p>
@@ -30,5 +31,13 @@ const SingUp: NextPage = () => {
 export default SingUp
 
 const Container = styled.div`
-
-`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 650px;
+  ${media.lessThan("medium")`
+      width:125%;
+    `}
+`;
