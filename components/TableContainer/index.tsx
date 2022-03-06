357db81037c0012bb2f83bc9753366e611015500
João Otávio <joaotavioos@outlook.com>
2022-03-06 10:33:37 -0300

    Add ordres fetcher

--- components/TableContainer/index.tsx
+++ components/TableContainer/index.tsx
@@ -1,33 +1,47 @@
 import { TableContainerTag } from "./styles";
 import Table from "../Table";
 import Modal from "../TableOptions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaTimesCircle } from "react-icons/fa";
-import TablesService from "services/tables";
-import useSWR from "swr";
-import { toast } from "react-toastify";
 import TableType from "dtos/Table";
+import OrderService from "services/order";
+import OrdersList from "dtos/OrdersList";
+import Order from "dtos/Order";
 
 type Props = {
-  data: [TableType]
-}
+  data: [TableType];
+};
 const TableContainer = ({ data }: Props) => {
   const [show, setShow] = useState(false);
   const [number, setNumber] = useState("");
+  const [orders, setOrders] = useState<OrdersList[]>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const orders = await OrderService.index();
+      setOrders(orders);
+    }
+    fetchData();
+  }, []);
 
   function handleClick(e) {
     setShow(!show);
     setNumber(e.target.text);
   }
 
-  const element = data.find(item => item.table_number.toString() === number.split(' ')[1])
-  const avaliable = element && element.avaliable_table
-
+  const element = data.find(
+    (item) => item.table_number.toString() === number.split(" ")[1]
+  );
+  const avaliable = element && element.avaliable_table;
 
   return (
     <>
       {show && (
-        <Modal number={number} disabled={avaliable} >
+        <Modal
+          orders={orders as unknown as [Order]}
+          number={number}
+          disabled={avaliable}
+        >
           {number}
           <span onClick={() => setShow(false)}>
             <FaTimesCircle size="28" />
--- components/TableOptions/index.tsx
+++ components/TableOptions/index.tsx
@@ -1,19 +1,49 @@
 import { Modal, ModalOverlay } from "./styles";
 import { Button } from "react-bootstrap";
 import { useRouter } from "next/router";
+import Order from "dtos/Order";
 
-const TableOptions = ({ children, number, disabled }) => {
+type Props = {
+  children: React.ReactNode;
+  number: string;
+  disabled: boolean;
+  orders: [Order];
+};
+
+const TableOptions: React.FC<Props> = ({
+  children,
+  number,
+  disabled,
+  orders,
+}) => {
   const router = useRouter();
+  const num = Number(number.split(" ")[1]);
+  const order = orders.find(
+    (order) => order.table_id === num && order.status === "Waiting"
+  );
+
   return (
     <>
       <ModalOverlay>
         <Modal>
           <h5>{children}</h5>
           <p>Selecione uma opção</p>
-          <Button variant="custom-orange" className="text-white my-3" size="lg">
-            {disabled ? "Abrir" : "Fechar"} Mesa
+          <Button
+            variant="custom-orange"
+            className="text-white my-3"
+            size="lg"
+            onClick={() => {}}
+          >
+            {!disabled ? "Abrir" : "Fechar"} Mesa
           </Button>
-          <Button variant="custom-orange" className="text-white my-3" size="lg">
+          <Button
+            variant="custom-orange"
+            className="text-white my-3"
+            size="lg"
+            onClick={() =>
+              disabled ? router.push(`/tableOrder/${num}`) : null
+            }
+          >
             Adicionar Pedido
           </Button>
           <Button
@@ -21,7 +51,9 @@ const TableOptions = ({ children, number, disabled }) => {
             variant="custom-orange"
             className="text-white my-3"
             size="lg"
-            onClick={() => router.push(`tableOrder/${number.split(" ")[1]}`)}
+            onClick={() => {
+              router.push(`/update?table=${num}&order=${order.id}`);
+            }}
           >
             Consultar Pedidos da mesa
           </Button>
