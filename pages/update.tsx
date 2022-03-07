8e020b37061571c875ed5fe66137baa0fc4f970a
João Otávio <joaotavioos@outlook.com>
2022-03-07 09:00:33 -0300

    Finish layout for update page

--- pages/update.tsx
+++ pages/update.tsx
@@ -1,16 +1,34 @@
 import Header from "components/Header";
+import { TableContainerTag } from "components/TableContainer/styles";
+import withAuth from "components/withAuth";
 import Link from "next/link";
-import { Container } from "react-bootstrap";
-import { BsChevronLeft } from "react-icons/bs";
+import { Button, Container, Table } from "react-bootstrap";
+import { BsChevronLeft, BsTrash } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import OrderService from "services/order";
+import useSwr from "swr";
+import media from "styled-media-query";
+import styled from "styled-components";
 
 type Props = {
-  table: number;
-  order: number;
+  table?: number;
+  order?: number;
 };
+
 const Update: React.FC<Props> = ({ table, order }) => {
-  console.log(table, order);
+  const { data, error } = useSwr(
+    `/storefront/v1/orders/${order}`,
+    OrderService.show
+  );
+
+  if (error) {
+    toast.error("Erro ao obter dados do pedido");
+    console.log(error);
+  }
+
   return (
-    <>
+    <Wrapper>
       <Header>
         <Link href="/tables">
           <a style={{ color: "white", textDecoration: "none" }}>
@@ -19,18 +37,83 @@ const Update: React.FC<Props> = ({ table, order }) => {
         </Link>
         <span>Mesa {table}</span>
       </Header>
-      <Container></Container>
-    </>
+      <Container>
+        <p> Items do pedido de {data?.client_name}</p>
+        <TableContainerTag>
+          <Table responsive>
+            <thead>
+              <tr style={{ color: "white" }}>
+                <th>Quantidade</th>
+                <th>Produto</th>
+                <th>Subtotal</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            {data?.order_items_attributes.map((order) => (
+              <tbody key={order.id}>
+                <tr style={{ color: "white" }}>
+                  <td>{order.quantity}</td>
+                  {console.log(order.products)}
+
+                  <td>{order.products.name}</td>
+                  <td>{order.products.price * order.quantity}</td>
+                  <td>
+                    <BsTrash
+                      color="white"
+                      onClick={() => {}}
+                      style={{ cursor: "pointer" }}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </Table>
+        </TableContainerTag>
+        <Button variant="custom-orange">
+          NOVO PEDIDO P/{" "}
+          {data?.client_name?.split(" ")[0]
+            ? data?.client_name?.split(" ")[0].toUpperCase()
+            : data?.client_name?.toUpperCase()}
+        </Button>
+      </Container>
+    </Wrapper>
   );
 };
 
 export async function getServerSideProps({ query }) {
-  return {
-    props: {
-      table: query.table,
-      order: query.order,
-    },
-  };
+  return { props: { table: query.table, order: query.order } };
 }
 
-export default Update;
+export default withAuth(Update);
+
+const Wrapper = styled.div`
+  p {
+    text-align: center;
+    margin-top: 2vw;
+    font-size: 15px;
+  }
+
+  button {
+    width: 250px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px auto;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+    color: white;
+    font-size: 15px;
+  }
+
+  ${media.greaterThan("medium")`
+  .table-responsive {
+      width: 70%;
+  }
+  p {
+    font-size: 25px;
+
+  }
+  
+    `}
+`;
