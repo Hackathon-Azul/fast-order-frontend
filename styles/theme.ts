9d973d9fd9e6cbfea2234dfefbad3e6c71e6dad0
João Otávio <joaotavioos@outlook.com>
2022-03-13 18:27:08 -0300

    Update theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -4,6 +4,9 @@ export default {
       header: "#014E70",
       orange: "#EC5D37",
       navy: "#141A32",
-      green: "#27AE60"
+      green: "#27AE60",
+      background: "#2C3147",
+      white: "#FFF",
+      text: "#2A628F"
     },
 } as const
